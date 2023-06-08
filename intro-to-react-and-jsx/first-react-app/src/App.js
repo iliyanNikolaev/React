38226d982e526c16553523902725bc4e6e73a183
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-06-08 18:34:07 +0300

    clear necessary files, prep for next theme

--- intro-to-react-and-jsx/first-react-app/src/App.js
+++ intro-to-react-and-jsx/first-react-app/src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
+import { Logo } from './components/Logo';
+import { Paragraph } from './components/Paragraph';
+import { Anchor } from './components/Anchor';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
