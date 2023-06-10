8bf9c80a8d8fde0672f54bf0aa1254c9edb4bc38
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-06-10 10:48:29 +0300

    preparing files for components demo

--- intro-to-react-and-jsx/first-react-app/src/components/Counter.js
+++ intro-to-react-and-jsx/first-react-app/src/components/Counter.js
@@ -2,7 +2,7 @@ import React from "react"
 
 const Counter = (props) => {
     let [counter, setCounter] = React.useState(0);
-    
+
     const incrementClick = (e) => {
         setCounter(oldCounter => oldCounter + 1);
     }
@@ -15,26 +15,26 @@ const Counter = (props) => {
         setCounter(0);
     }
 
-    if(counter > 5){
+    if (counter > 5) {
         counter = 0;
     }
 
     return <article>
         <h3>{getTitle(counter)} .. {counter}</h3>
-        {counter < 5 
-        ? <button onClick={incrementClick}>Increment</button>
-        : null}
+        {counter < 5
+            ? <button onClick={incrementClick}>Increment</button>
+            : null}
         <button onClick={resetClick}>Reset</button>
-        {counter !== 0 
-        ? <button onClick={decrementClick}>Decrement</button>
-        : null}
-        
+        {counter !== 0
+            ? <button onClick={decrementClick}>Decrement</button>
+            : null}
+
 
     </article>
 }
 
-function getTitle(counter){
-    switch (counter){
+function getTitle(counter) {
+    switch (counter) {
         case 0: return 'Welcome to Mobile Legends!';
         case 1: return 'Enemy has been slain!';
         case 2: return 'Double kill!';
