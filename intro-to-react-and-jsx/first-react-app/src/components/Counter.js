1ff43cdbc4ac607a7eda9b5df801db608fa59489
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-06-10 09:43:06 +0300

    conditional rendering

--- intro-to-react-and-jsx/first-react-app/src/components/Counter.js
+++ intro-to-react-and-jsx/first-react-app/src/components/Counter.js
@@ -15,13 +15,34 @@ const Counter = (props) => {
         setCounter(0);
     }
 
+    if(counter > 5){
+        counter = 0;
+    }
+
     return <article>
-        <h3>Counter {counter}</h3>
-        <button onClick={incrementClick}>Increment</button>
+        <h3>{getTitle(counter)} .. {counter}</h3>
+        {counter < 5 
+        ? <button onClick={incrementClick}>Increment</button>
+        : null}
         <button onClick={resetClick}>Reset</button>
-        <button onClick={decrementClick}>Decrement</button>
+        {counter !== 0 
+        ? <button onClick={decrementClick}>Decrement</button>
+        : null}
+        
 
     </article>
+}
+
+function getTitle(counter){
+    switch (counter){
+        case 0: return 'Welcome to Mobile Legends!';
+        case 1: return 'Enemy has been slain!';
+        case 2: return 'Double kill!';
+        case 3: return 'Triple kill!';
+        case 4: return 'MANIAC!!';
+        case 5: return 'SAVAGEEE!!!!';
+        default: return 'Out of range';
+    }
 }
 
 export default Counter;
