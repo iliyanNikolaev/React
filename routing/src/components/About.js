58b834ad776e2e64665fbd721dd25732a393bc33
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-06-18 20:44:45 +0300

    basic example how to use react-router-dom

---
+++ routing/src/components/About.js
@@ -0,0 +1,8 @@
+const About = () => {
+    return <>
+        <h2>About us</h2>
+        <p>Phone: +5857-002-227</p>
+    </>
+}
+
+export default About;
---
+++ routing/src/components/Home.js
@@ -0,0 +1,8 @@
+const Home = (props) => {
+    return <>
+        <h2>Home page</h2>
+        <p>Welcome to our site</p>
+    </>
+}
+
+export default Home;
