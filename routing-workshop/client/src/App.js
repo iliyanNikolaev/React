681afd664ae7f4775d86efcd32f092b974e04fac
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-06-21 20:42:48 +0300

    setup routes with react-router-dom

--- routing-workshop/client/src/App.js
+++ routing-workshop/client/src/App.js
@@ -1,12 +1,25 @@
+import { Routes, Route } from 'react-router-dom';
+
 import './App.css';
 import { Header } from './components/Header/Header';
 import { Home } from './components/Home/Home';
+import { Login } from './components/Login/Login';
+import { Register } from './components/Register/Register';
+import { Catalog } from './components/Catalog/Catalog';
+import { Create } from './components/Create/Create';
 
 function App() {
     return (
         <div id="box">
             <Header />
-            <Home />
+
+            <Routes>
+                <Route path='/' element={<Home />}></Route>
+                <Route path='/login' element={<Login />}></Route>
+                <Route path='/register' element={<Register />}></Route>
+                <Route path='/catalog' element={<Catalog />}></Route>
+                <Route path='/create' element={<Create />}></Route>
+            </Routes>
         </div>
     );
 }
