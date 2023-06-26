90b9f9565e0c5325d771b81ebe19b900f138d397
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-06-26 21:52:19 +0300

    show edit and delete buttons to the owner only

--- routing-workshop/client/src/components/Details/Details.js
+++ routing-workshop/client/src/components/Details/Details.js
@@ -7,7 +7,9 @@ import { AuthContext } from "../../contexts/authContext";
 export const Details = (props) => {
   const { gameId } = useParams(); // така се взима ид-то на конкретната игра което сме настроили в раутовете в аппа
   const [currentGame, setCurrentGame] = React.useState({});
+
   const [comments, setComments] = React.useState([]);
+
   const [currentComment, setCurrentComment] = React.useState('');
 
   const { auth } = React.useContext(AuthContext);
@@ -35,55 +37,64 @@ export const Details = (props) => {
     }
 
     const comment = await postCommentForGame(commentData, auth.accessToken);
-    
+
     setComments(state => ([...state, comment]));
     setCurrentComment('');
   }
 
-   return <> 
-   <section id="game-details">
-    <h1>Game Details</h1>
-    <div className="info-section">
-      <div className="game-header">
-        <img className="game-img" src={currentGame.imageUrl} alt="game-poster"/>
-        <h1>{currentGame.title}</h1>
-        <span className="levels">MaxLevel: {currentGame.maxLevel}</span>
-        <p className="type">{currentGame.category}</p>
-      </div>
-      <p className="text">
-        {currentGame.summary}
-      </p>
+  return <>
+    <section id="game-details">
+      <h1>Game Details</h1>
+      <div className="info-section">
+        <div className="game-header">
+          <img className="game-img" src={currentGame.imageUrl} alt="game-poster" />
+          <h1>{currentGame.title}</h1>
+          <span className="levels">MaxLevel: {currentGame.maxLevel}</span>
+          <p className="type">{currentGame.category}</p>
+        </div>
+        <p className="text">
+          {currentGame.summary}
+        </p>
 
-      <CommentsList comments={comments} />
-      
-      {/* Edit/Delete buttons ( Only for creator of this game )  */}
-      <div className="buttons">
-        <a href="/" className="button">
-          Edit
-        </a>
-        <a href="/" className="button">
-          Delete
-        </a>
+        <CommentsList comments={comments} />
+
+        {/* Edit/Delete buttons ( Only for creator of this game )  */}
+
+        {auth._id === currentGame._ownerId
+          ? <div className="buttons">
+            <a href="/" className="button">
+              Edit
+            </a>
+            <a href="/" className="button">
+              Delete
+            </a>
+          </div>
+          : null}
+
       </div>
-    </div>
-    {/* Bonus */}
-    {/* Add Comment ( Only for logged-in users, which is not creators of the current game ) */}
-    <article className="create-comment">
-      <label>Add new comment:</label>
-      <form className="form" onSubmit={onCommentSubmit}>
-        <textarea
-          name="comment"
-          placeholder="Comment......"
-          value={currentComment}
-          onChange={onCommentChange}
-        />
-        <input
-          className="btn submit"
-          type="submit"
-          defaultValue="Add Comment"
-        />
-      </form>
-    </article>
-  </section>
+      {/* Bonus */}
+      {/* Add Comment ( Only for logged-in users, which is not creators of the current game ) */}
+
+      { auth._id !== undefined && auth._id !== currentGame._ownerId
+        ? <article className="create-comment">
+          <label>Add new comment:</label>
+          <form className="form" onSubmit={onCommentSubmit}>
+            <textarea
+              name="comment"
+              placeholder="Comment......"
+              value={currentComment}
+              onChange={onCommentChange}
+            />
+            <input
+              className="btn submit"
+              type="submit"
+              defaultValue="Add Comment"
+            />
+          </form>
+        </article>
+        : null
+      }
+
+    </section>
   </>
 }
