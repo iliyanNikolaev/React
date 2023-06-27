fb9c0f2221ebcb56cb2484c8833fde3e40bfe315
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-06-27 12:45:41 +0300

    created header and requirements components

--- ppl-app-prototype/src/components/Header/Header.js
+++ ppl-app-prototype/src/components/Header/Header.js
@@ -1,5 +1,12 @@
 import { Link } from "react-router-dom"
 
 export const Header = (props) => {
-    
+    return (
+        <nav>
+            <Link to='/'>PPL(A)</Link>
+            <Link to='/requirements'>Изисквания</Link>
+            <Link to='/structure'>Структура на курса</Link>
+            <Link to='/exams'>Изпити и процедури</Link>
+        </nav>
+    );
 }
