6b894a591bf25cff06ffd6bce6ddeda6e23331a7
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-06-27 12:45:16 +0300

    added react-router-dom to the project

--- ppl-app-prototype/src/App.js
+++ ppl-app-prototype/src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 
+import {Routes, Route} from 'react-router-dom';
+
+import { Header } from './components/Header/Header';
+import { Requirements } from './components/Requirements/Requirements';
+
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+	return (
+		<div className="App">
+			<Header />
+
+			<Routes>
+				<Route path='/requirements' element={<Requirements />}></Route>
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
