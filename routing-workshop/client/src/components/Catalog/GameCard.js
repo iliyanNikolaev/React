5f644c9f673bd99b2d56573b713e12791f144798
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-07-03 09:29:39 +0300

    some component tests

--- routing-workshop/client/src/components/Catalog/GameCard.js
+++ routing-workshop/client/src/components/Catalog/GameCard.js
@@ -6,9 +6,9 @@ export const GameCard = ({
     return <>
         <div className="allGames">
             <div className="allGames-info">
-                <img src={game.imageUrl} alt="game-poster"/>
+                <img src={game.imageUrl} alt="game-poster" />
                 <h6>{game.category}</h6>
-                <h2 id="game-title">{game.title}</h2>
+                <h2 data-testid="game-title">{game.title}</h2>
                 <Link to={`/details/${game._id}`} className="details-button">
                     Details
                 </Link>
--- routing-workshop/client/src/components/Catalog/GameCard.test.js
+++ routing-workshop/client/src/components/Catalog/GameCard.test.js
@@ -1,18 +1,32 @@
-import ReactDOM from 'react-dom/client';
+import { getByText, queryByText, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import { BrowserRouter } from 'react-router-dom';
 import { GameCard } from "./GameCard";
+
+
 describe('GameCard component', () => {
-
     test('GameCard title', () => {
-        const expectTitle = 'test-title';
-        const div = document.createElement('div');
-        const root = ReactDOM.createRoot(div);
-
-        document.appendChild(root);
-
-        root.render(<GameCard game={{ title: 'test-title' }} />);
-
-        const actualTitle = document.querySelector('#game-title').value;
-
-        expect(actualTitle).toBe.expectTitle;
+        render(
+            <BrowserRouter>
+                <GameCard game={{ title: 'test-title' }} />
+            </BrowserRouter>
+        );
+        expect(screen.getByTestId('game-title')).toHaveTextContent('test-title');
     });
+
+    test('Click on details', async () => {
+        const game = {
+            _id: 'test-id'
+        }
+
+        render(
+            <BrowserRouter>
+                <GameCard game={game} />
+            </BrowserRouter>
+        );
+
+        await userEvent.click(screen.queryByText('Details'));
+
+        expect(document.location.pathname).toContain(`/details/${game._id}`);
+    })
 })
