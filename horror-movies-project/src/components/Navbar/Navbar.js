fe698acab25b645e63020df47a0077fdf9b45875
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-07-16 13:15:50 +0300

    created navbar component

--- horror-movies-project/src/components/Navbar/Navbar.js
+++ horror-movies-project/src/components/Navbar/Navbar.js
@@ -1,17 +1,32 @@
 import React from 'react'
+import './Navbar.css'
 import { Link } from 'react-router-dom'
 
 export const Navbar = (props) => {
     return (
         <>
-            <nav>
-                <ul>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/catalog'>Catalog</Link></li>
-                    <li><Link to='/login'>Login</Link></li>
-                    <li><Link to='/register'>Register</Link></li>
-                </ul>
-            </nav>
+            <header className="header">
+                <img
+                    className="logo"
+                    src="horror-logo.png"
+                    alt="logo"
+                />
+                <nav className="navigation">
+                    <ul className="list">
+                        <li className='nav-link'><Link to='/'>Home</Link></li>
+                        <li className='nav-link'><Link to='/catalog'>Catalog</Link></li>
+                        <li className='nav-link'><Link to='/login'>Login</Link></li>
+                        <li className='nav-link'><Link to='/register'>Register</Link></li>
+                    </ul>
+                </nav>
+                <button className="btn search">
+                    <i className="fas fa-search" />
+                </button>
+                <button className="btn profile">
+                    <i className="fas fa-user-circle" />
+                </button>
+            </header>
+
         </>
     )
 }
