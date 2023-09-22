7610e87dcccc2c719a374e12e5a0e4354610b1b5
Iliyan Nikolaev <ilichVIVA@gmail.com>
2023-09-22 15:46:57 +0300

    home page

--- lamadev-nextjs-mongo/src/app/page.jsx
+++ lamadev-nextjs-mongo/src/app/page.jsx
@@ -1,6 +1,21 @@
+import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
   return (
-   <div>Hello</div>
+    <div className={styles.container}>
+      <div className={styles.texts}>
+        <h1 className={styles.title}>I'm Iliyan and i learn Next.js while developing this app</h1>
+
+        <p className={styles.desc}>Next.js: JavaScript framework for fast web apps, built on React, with server-side rendering, pre-rendering, and intuitive routing for optimal performance.</p>
+
+        <button className={styles.button}>
+          see my other work
+        </button>
+      </div>
+      <div className={styles.img}>
+        <Image src={'/keyboardmonitor.png'} width={524} height={486} alt='dashimg' />
+      </div>
+    </div>
   )
 }
